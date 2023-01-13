29fc33089d7fcab6031525d4ba56f7843e9543e5
irkham <irkham.nurhadi.13@gmail.com>
2023-01-13 15:13:27 +0700

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,73 @@
+import colors from 'vuetify/es5/util/colors';
+
+export default {
+  ssr: false,
+  server: {
+    port: 5454,
+  },
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - irkham',
+    title: 'irkhamdev',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: ['@nuxtjs/axios', '@nuxtjs/proxy'],
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    treeShake: true,
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
+    },
+  },
+  axios: {
+    proxy: true,
+  },
+  proxy: {
+    // target: 'http://localhost:5000/',
+    '/api/': {
+      target: 'http://localhost:5000/',
+      pathRewrite: { "^/api/": "" }
+    },
+  },
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+};
---
+++ pages/register.vue
@@ -0,0 +1,99 @@
+<template>
+  <v-row>
+    <v-col cols="10" offset="1" md="4" offset-md="4">
+      <v-card>
+        <v-toolbar dark color="primary">Register</v-toolbar>
+
+        <v-card-text>
+          <v-form ref="form">
+            <v-text-field label="Name" :rules="rules.fullname" v-model="form.fullname" required>
+            </v-text-field>
+
+            <v-text-field label="Email" :rules="rules.email" v-model="form.email" @keydown="resetEmailExistMessage"
+              required>
+            </v-text-field>
+
+            <v-text-field label="Password" :rules="rules.password" v-model="form.password" type="password" required>
+            </v-text-field>
+
+            <v-text-field label="Confirm Password" :rules="rules.password_confirmation"
+              v-model="form.password_confirmation" type="password" required>
+            </v-text-field>
+
+          </v-form>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn :loading="isLoading" color="primary" @click="onSubmit">Register</v-btn>
+        </v-card-actions>
+      </v-card>
+      <div class="d-flex align-baseline">
+        <p>Sudah punya akun?</p>
+        <v-btn text plain to="/login" :ripple="false" color="primary" class="pl-1">LOGIN</v-btn>
+      </div>
+
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  layout: 'auth',
+  data() {
+    return {
+      emailExist: false,
+      isLoading: false,
+      form: {
+        fullname: '',
+        email: '',
+        password: '',
+        password_confirmation: ''
+      },
+      rules: {
+        fullname: [
+          (v) => !!v || 'Fullname is required',
+        ],
+        email: [
+          (v) => !!v || 'Email is required',
+          (v) => /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/.test(v) || 'Email must be valid',
+          (v) => !this.emailExist || 'Email already exist',
+
+        ],
+        password: [
+          (v) => !!v || 'Password is required',
+          (v) => v.length >= 6 || 'Password must be at least 6 characters',
+        ],
+        password_confirmation: [
+          (v) => !!v || 'Password confirmation is required',
+          (v) => v === this.form.password || 'Password confirmation must be same with password'
+        ]
+      }
+    }
+  },
+  methods: {
+    resetEmailExistMessage() {
+      this.emailExist = false
+    },
+    async onSubmit() {
+      try {
+        if (this.$refs.form.validate()) {
+          this.isLoading = true;
+          const response = await this.$axios.$post('/api/register', this.form);
+          
+          if(response.message === 'user is created succesfully') {
+            alert(response.message);
+          }
+          this.isLoading = false;
+        }
+      } catch (error) {
+        console.log(error.response);
+        if (error.response.data.message === 'Email is already registered') {
+          this.emailExist = true;
+          this.$refs.form.validate();
+        }
+      }
+    }
+  }
+}
+</script>
