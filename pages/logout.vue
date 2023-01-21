06c122d1b69078074ac9022367f35bfc9ee1e7ce
irkham <irkham.nurhadi.13@gmail.com>
2023-01-21 17:38:21 +0700

    remove cookies permanently

--- pages/logout.vue
+++ pages/logout.vue
@@ -1,7 +1,16 @@
 <script>
+import * as Cookies from 'js-cookie';
+
 export default {
   middleware: ['authenticated'],
   mounted() {
+    // pilih salah satu
+    
+    // hapus cookies permanen
+    // Cookies.remove('vuex');
+    // window.location.href = '/login';
+
+    // set cookies = null
     this.$store.commit('auth/logout'),
     this.$router.push({name: 'index___' + this.$i18n.locale})
   }
