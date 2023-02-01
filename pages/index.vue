b303375d11704201e94084cda9e4d92fbd4877f4
irkham <irkham.nurhadi.13@gmail.com>
2023-02-01 09:06:15 +0700

    New Form Button

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,12 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
+  <v-container class="mt-3">
+    <v-row>
+    <v-col cols="12" class="text-right">
+      <BtnCreateForm/>
     </v-col>
   </v-row>
+  </v-container>
+  
 </template>
 
 <script>
