2e275f2e97c4b544c97d84c3af19cc88dbba475a
irkhamissimo <56974703+irkhamissimo@users.noreply.github.com>
2023-02-02 16:52:50 +0700

    Questions and Answers toolbar

---
+++ components/Toolbar.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-toolbar flat height="40px">
+    <v-spacer></v-spacer>
+    <v-btn plain :ripple="false" to="/">Questions</v-btn>
+    <v-btn plain :ripple="false" to="/">Response</v-btn>
+    <v-spacer></v-spacer>
+  </v-toolbar>
+</template>
