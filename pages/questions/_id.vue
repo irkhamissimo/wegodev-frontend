ec1296cf45c72718266dd15c27239b997107d091
irkhamissimo <56974703+irkhamissimo@users.noreply.github.com>
2023-02-02 20:49:48 +0700

    questions page

---
+++ pages/questions/_id.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-container fluid class="pa-0">
+      <Toolbar />
+    </v-container>
+
+    <v-container>
+      <v-row>
+        <v-col md="8" offset-md="2" sm="10" offset-sm="1">
+          <v-card>
+            <v-card-text>
+              question1
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col md="8" offset-md="2" sm="10" offset-sm="1">
+          <v-card>
+            <v-card-text>
+              question2
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+
+</template>
