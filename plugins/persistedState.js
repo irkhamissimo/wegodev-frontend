d84d32d2ac2096898813def1893473f8c1b818d2
irkham <irkham.nurhadi.13@gmail.com>
2023-02-02 08:21:14 +0700

    cookies 1 day only

--- plugins/persistedState.js
+++ plugins/persistedState.js
@@ -17,7 +17,7 @@ export default ({ store, req }) => {
             },
             // Please see https://github.com/js-cookie/js-cookie#json, on how to handle JSON.
             setItem: (key, value) =>
-                Cookies.set(key, value, { expires: 365, secure: false }),
+                Cookies.set(key, value, { expires: 1, secure: false }),
             removeItem: key => Cookies.remove(key)
         }
     })(store);
